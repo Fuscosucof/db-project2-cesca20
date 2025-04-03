608d3a8a64d23ebb14e0d3f15a0911946a1776ce
Natthanita <natthanita.suradet@gmail.com>
2025-04-03 15:57:34 +0700

    frontend

---
+++ Js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('magicalForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Your magical details have been sent to the wizarding council!');
+});
